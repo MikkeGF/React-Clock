804bd216adb4b9c5f77d1ed1f7f67b33325dbf80
MikkeGF <n9pemi00students.oamk.fi>
2020-05-12 16:03:24 +0300

    valmis gittiin

--- src/Clock.js
+++ src/Clock.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 export default class Clock extends React.Component {
-  constructor ()
+  constructor (props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(),1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({date: new Date()});
+  }
   render() {
     return (
-
+      <div>{this.state.date.toLocaleTimeString()}</div>
     )
   }
+
 }
